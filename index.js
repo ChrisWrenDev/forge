56f9151b2f671a6ffce48febf9d296825aae1d5c
Chris Wren <dazedpandas@gmail.com>
2024-03-26 21:13:37 +0000

    chore: Initial project setup

---
+++ index.js
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+
+const content = fs.readFileSync("./app.fgx", "utf-8");
+const ast = parse(content);
+
+const analysis = analyse(ast);
+const js = generate(ast, analysis);
+
+fs.writeFileSync("./app.js", js, "utf-8");
+
+function parse(content) {}
+function analyse(ast) {}
+function generate(ast, analysis) {}
